cfd5f4b195dda0351f41415e663be4b6f116418d
julian-patterson <julianedwardpatterson@gmail.com>
2024-05-28 21:21:08 -0400

    adding state to hold last color & brightness

--- src/strip.rs
+++ src/strip.rs
@@ -6,11 +6,18 @@ use std::sync::{Once, Mutex};
 mod colors;
 use colors_transform::Hsl;
 use colors_transform::Color;
-use colors_transform::Rgb;
 
 static INIT: Once = Once::new();
 static mut STRIP: Option<Mutex<Controller>> = None;
-// static ref LAST_COLORS: Mutex<Vec<(i32,i32,i32,i32)>> = Mutex::new(Vec::new());
+
+pub struct LastState {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+static mut LAST_STATE: Option<LastState> = None;
+static mut LAST_STATE_BRIGHTNESS: u8 = 255;
+
 
 pub fn init() {
     INIT.call_once(|| {
@@ -41,10 +48,24 @@ pub async fn strip_on() {
     let mut controller = unsafe {
         STRIP.as_ref().unwrap().lock().unwrap()
     };
+    
+    let color = unsafe {
+        LAST_STATE.as_ref().unwrap_or(&LastState {
+            r: 255,
+            g: 255,
+            b: 255,
+        })
+    };
+
+    let brightness = unsafe {
+        LAST_STATE_BRIGHTNESS
+    };
+    
+    controller.set_brightness(0, brightness);
 
     let leds = controller.leds_mut(0);
     for led in leds {
-        *led = [0, 0, 255, 0];
+        *led = [color.g , color.b, color.r, 100];
     }
 
     controller.render().unwrap();
@@ -70,6 +91,10 @@ pub fn strip_set_brightness(brightness: i32) {
 
     let u_brightness = brightness as u8;
 
+    unsafe {
+        LAST_STATE_BRIGHTNESS = u_brightness;
+    }
+
     controller.set_brightness(0,u_brightness);
     controller.render().unwrap();
 }
@@ -88,14 +113,19 @@ pub fn strip_set_hsv(h: f32, s: f32, v: f32) {
     let g = rgb.get_green() as u8;
     let b = rgb.get_blue() as u8;
 
-    println!("Setting color to: {}, {}, {}", r, g, b);
+    unsafe {
+        LAST_STATE = Some(LastState {
+            r: r,
+            g: g,
+            b: b,
+        });
+    }
 
     let leds = controller.leds_mut(0);
     for led in leds {
         *led = [g, b, r, 100];
     }
 
-    let all_leds = controller.leds(0);
     controller.render().unwrap();
 }
 
@@ -106,40 +136,18 @@ pub fn strip_set_temperature(temperature: i32) {
 
     let (r,g,b) = colors::convert_k_to_rgb(colors::convert_ha_temperature(temperature));
 
+    unsafe {
+        LAST_STATE = Some(LastState {
+            r: r,
+            g: g,
+            b: b,
+        });
+    }
+
     let leds = controller.leds_mut(0);
     for led in leds {
         *led = [g, b, r, 100];
     }
 
-    let all_leds = controller.leds(0);
     controller.render().unwrap();
 }
-
-
-// pub fn main() {
-//     let mut controller = ControllerBuilder::new()
-//     .freq(800_000)
-//     .dma(10)
-//     .channel(
-//         0, // Channel Index
-//         ChannelBuilder::new()
-//             .pin(18) // GPIO 10 = SPI0 MOSI
-//             .count(100) // Number of LEDs
-//             .strip_type(StripType::Ws2812)
-//             .brightness(255) // default: 255
-//             .invert(false)
-//             .build(),
-//     )
-//     .build()
-//     .unwrap();
-
-//     let leds = controller.leds_mut(0);
-
-//     for led in leds {
-//         *led = [0, 0, 255, 0];
-//     }
-
-//     let all_leds = controller.leds(0);
-
-//     controller.render().unwrap();
-// }
--- src/strip/colors.rs
+++ src/strip/colors.rs
@@ -1,5 +1,3 @@
-use std::f32::consts::E;
-
 
 pub fn convert_ha_temperature(mut temperature: i32) -> i32 {
     if temperature < 154 {
